f9bcdfcea8be728a1a28d174e822d77e6db5980c
MrNibo99 <leokhani99@outlook.com>
2024-10-28 17:42:17 +0100

    commit

--- components/Hero/HeroClient.tsx
+++ components/Hero/HeroClient.tsx
@@ -19,7 +19,7 @@ const HeroClient = () => {
     <>
       <div className="relative flex h-screen max-w-7xl w-full mx-auto flex-col gap-10 md:gap-20 lg:gap-40 items-center justify-center overflow-hidden bg-background">
         <div className="flex flex-col md:flex-row justify-between items-start gap-2 w-full">
-          {/* Left Side */}
+          {/* Left Side  */}
           <div className="flex flex-col items-center md:items-start gap-2 w-full md:w-2/5">
             <div className="w-full text-4xl md:text-6xl lg:text-7xl uppercase font-semibold text-center md:text-left leading-tight break-words">
               {heroItems.title}
