243414f7b582c742e597accbbf170ff01bfd7ee0
MrNibo99 <leokhani99@outlook.com>
2024-11-02 11:53:35 +0100

    fix contact us

--- components/ContactUs/ContactUsClient.tsx
+++ components/ContactUs/ContactUsClient.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
@@ -15,48 +13,11 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import Image from "next/image";
-import { toast } from "@/hooks/use-toast";
 import { TitleSection } from "../ui/titles";
+import Image from "next/image";
 
-const formSchema = z.object({
-  name: z.string().min(2, { message: "Name must be at least 2 characters." }),
-  email: z.string().email({ message: "Please enter a valid email address." }),
-  phone: z.string().regex(/^\+?[1-9]\d{1,14}$/, {
-    message: "Please enter a valid phone number.",
-  }),
-  message: z
-    .string()
-    .min(10, { message: "Message must be at least 10 characters." }),
-});
-
-export default function ContactUsClient() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      email: "",
-      phone: "",
-      message: "",
-    },
-  });
-
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    setIsSubmitting(true);
-    // Simulate API call
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    setIsSubmitting(false);
-    toast({
-      title: "Message Sent",
-      description:
-        values.message +
-        "We've received your message and will get back to you soon.",
-    });
-
-    form.reset();
-  }
+function ContactUsClient() {
+  const [state, handleSubmit] = useForm("xnnqpgje");
 
   return (
     <section className="py-12">
@@ -65,82 +26,111 @@ export default function ContactUsClient() {
         <div className="flex md:justify-between justify-center items-center gap-8">
           <div className="p-6 w-1/2 max-w-xl">
             <h3 className="text-xl font-semibold mb-4">Send us a message</h3>
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-4"
-              >
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Name</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Your name" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="email"
-                          placeholder="Your email"
-                          {...field}
+            {state.succeeded ? (
+              <p className="text-green-500">
+                Thanks for reaching out! We will get back to you soon.
+              </p>
+            ) : (
+              <Form>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                  {/* Name Field */}
+                  <FormField
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                          <Input id="name" placeholder="Your name" {...field} />
+                        </FormControl>
+                        <ValidationError
+                          prefix="Name"
+                          field="name"
+                          errors={state.errors}
                         />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="phone"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Phone</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="tel"
-                          placeholder="Your phone number"
-                          {...field}
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {/* Email Field */}
+                  <FormField
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            id="email"
+                            type="email"
+                            placeholder="Your email"
+                            {...field}
+                          />
+                        </FormControl>
+                        <ValidationError
+                          prefix="Email"
+                          field="email"
+                          errors={state.errors}
                         />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="message"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Message</FormLabel>
-                      <FormControl>
-                        <Textarea placeholder="Your message" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button
-                  type="submit"
-                  className="w-full bg-blue-500 hover:bg-blue-600 text-white text-md"
-                  disabled={isSubmitting}
-                >
-                  {isSubmitting ? "Sending..." : "Send Message"}
-                </Button>
-              </form>
-            </Form>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {/* Phone Field */}
+                  <FormField
+                    name="phone"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Phone</FormLabel>
+                        <FormControl>
+                          <Input
+                            id="phone"
+                            type="tel"
+                            placeholder="Your phone number"
+                            {...field}
+                          />
+                        </FormControl>
+                        <ValidationError
+                          prefix="Phone"
+                          field="phone"
+                          errors={state.errors}
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {/* Message Field */}
+                  <FormField
+                    name="message"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Message</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            id="message"
+                            placeholder="Your message"
+                            {...field}
+                          />
+                        </FormControl>
+                        <ValidationError
+                          prefix="Message"
+                          field="message"
+                          errors={state.errors}
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button
+                    type="submit"
+                    className="w-full bg-blue-500 hover:bg-blue-600 text-white text-md"
+                    disabled={state.submitting}
+                  >
+                    {state.submitting ? "Sending..." : "Send Message"}
+                  </Button>
+                </form>
+              </Form>
+            )}
           </div>
-          <div className=" p-6 rounded-lg justify-center items-center hidden md:flex">
+          <div className="p-6 rounded-lg justify-center items-center hidden md:flex">
             <Image
               src="/images/contact-us.svg"
               alt="Contact Us"
@@ -153,3 +143,5 @@ export default function ContactUsClient() {
     </section>
   );
 }
+
+export default ContactUsClient;
