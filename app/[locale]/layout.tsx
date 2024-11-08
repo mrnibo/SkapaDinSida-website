7b2d2ae8c1fdafa8f77f8d19de5bd10a1a988a7e
MrNibo99 <leokhani99@outlook.com>
2024-11-08 16:13:37 +0100

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -76,10 +76,11 @@ export default async function LocaleLayout({
         <script
           async
           id="clarity-script"
+          data-cookieconsent="statistics" // Load after consent for statistics
           dangerouslySetInnerHTML={{
             __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");`,
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");`,
           }}
         />
 
